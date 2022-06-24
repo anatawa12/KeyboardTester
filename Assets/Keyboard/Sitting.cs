ba991fe29e7f551879b64b78ba6a59d9797a35c6
anatawa12 <anatawa12@icloud.com>
2022-06-24 23:07:34 +0900

    feat: seat

---
+++ Assets/Keyboard/Sitting.cs
@@ -0,0 +1,14 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class Sitting : UdonSharpBehaviour
+{
+    private bool seated = false;
+    public override void Interact()
+    {
+        Networking.LocalPlayer.UseAttachedStation();
+    }
+}
