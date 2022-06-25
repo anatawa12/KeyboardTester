4e3bc1fad1aea10a558db23a6546a80e8d1eee09
anatawa12 <github@anatawa12.com>
2022-06-25 22:30:28 +0900

    chore: wide readme area

---
+++ Assets/Tester/Selecter.cs
@@ -0,0 +1,18 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class Selecter : UdonSharpBehaviour
+{
+    public GameObject enable;
+    public GameObject[] disables;
+
+    public override void Interact()
+    {
+        enable.SetActive(true);
+        foreach (var disable in disables)
+            disable.SetActive(false);
+    }
+}
