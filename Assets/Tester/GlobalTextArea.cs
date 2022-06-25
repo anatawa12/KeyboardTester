5b8a53fc3434444a2443eee50c000eacaace4878
anatawa12 <anatawa12@icloud.com>
2022-06-25 21:17:04 +0900

    feat: global textarea

---
+++ Assets/Tester/GlobalTextArea.cs
@@ -0,0 +1,31 @@
+﻿
+using TMPro;
+using UdonSharp;
+using VRC.SDKBase;
+
+public class GlobalTextArea : UdonSharpBehaviour
+{
+    public TextMeshPro globalArea;
+    public TextMeshPro sourceArea;
+    public TextMeshPro headerLine;
+
+    [UdonSynced]
+    public string text;
+
+    private void Update()
+    {
+        if (Networking.IsOwner(gameObject))
+        {
+            text = sourceArea.text;
+        }
+
+        globalArea.text = text;
+        var owner = Networking.GetOwner(gameObject);
+        headerLine.text = $"Global Textarea(owner: {(owner == null ? "None" : owner.displayName)})";
+    }
+
+    public override void Interact()
+    {
+        Networking.SetOwner(Networking.LocalPlayer, gameObject);
+    }
+}
