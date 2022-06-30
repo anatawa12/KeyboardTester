28136e9d38bbd89dfdb29b8bdfd8aac36d732a7a
anatawa12 <anatawa12@icloud.com>
2022-07-01 00:53:22 +0900

    feat: ring menu

---
+++ Assets/Keyboard/UI/UIRingController.cs
@@ -0,0 +1,95 @@
+﻿
+using System;
+using UdonSharp;
+
+public class UIRingController : KeyboardDisplay
+{
+    public RingCharsController singleChars;
+    public RingCharsController[] vectorSelections;
+    public LeftOrRight hand; 
+
+    [NonSerialized] public int Index = -2;
+    private char[] _table;
+    private string[] _seventhLine;
+    private int _mainRadius;
+    private int _subRadius;
+
+    private void Start()
+    {
+        switch (hand)
+        {
+            case LeftOrRight.Left:
+                _mainRadius = 8;
+                _subRadius = 1;
+                break;
+            case LeftOrRight.Right:
+                _mainRadius = 1;
+                _subRadius = 8;
+                break;
+            default:
+                ((object)null).ToString();
+                break;
+        }
+    }
+
+    public override void OnInput(int left, int right)
+    {
+        int main = -1;
+        int sub = -1;
+        switch (hand)
+        {
+            case LeftOrRight.Left:
+                main = left;
+                sub = right;
+                break;
+            case LeftOrRight.Right:
+                main = right;
+                sub = left;
+                break;
+            default:
+                ((object)null).ToString();
+                break;
+        }
+
+        if (sub == -1)
+        {
+            singleChars.Disable();
+            for (var mainI = 0; mainI < 8; mainI++)
+                vectorSelections[mainI].SetChars(
+                    _table[mainI * _mainRadius + 0 * _subRadius],
+                    _table[mainI * _mainRadius + 1 * _subRadius],
+                    _table[mainI * _mainRadius + 2 * _subRadius],
+                    _table[mainI * _mainRadius + 3 * _subRadius],
+                    _table[mainI * _mainRadius + 4 * _subRadius],
+                    _table[mainI * _mainRadius + 5 * _subRadius],
+                    _table[mainI * _mainRadius + 6 * _subRadius],
+                    _table[mainI * _mainRadius + 7 * _subRadius]);
+        }
+        else
+        {
+            foreach (var selection in vectorSelections)
+                selection.Disable();
+
+            singleChars.SetChars(
+                _table[0 * _mainRadius + sub * _subRadius],
+                _table[1 * _mainRadius + sub * _subRadius],
+                _table[2 * _mainRadius + sub * _subRadius],
+                _table[3 * _mainRadius + sub * _subRadius],
+                _table[4 * _mainRadius + sub * _subRadius],
+                _table[5 * _mainRadius + sub * _subRadius],
+                _table[6 * _mainRadius + sub * _subRadius],
+                _table[7 * _mainRadius + sub * _subRadius]);
+        }
+    }
+
+    public override void OnTableChanged(char[] newTable)
+    {
+        _table = newTable;
+    }
+}
+
+public enum LeftOrRight
+{
+    Left,
+    Right,
+}
