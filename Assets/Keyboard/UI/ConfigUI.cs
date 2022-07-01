ca46def5ec875941eb4bfbd84c75b8cf40866a85
anatawa12 <anatawa12@icloud.com>
2022-07-01 20:05:51 +0900

    feat: configuration UI

---
+++ Assets/Keyboard/UI/ConfigUI.cs
@@ -0,0 +1,17 @@
+﻿
+using UdonSharp;
+using UnityEngine.UI;
+
+public class ConfigUI : UdonSharpBehaviour
+{
+    public Keyboard keyboard;
+    public Slider sensitivity;
+    public Toggle triggerToInput;
+
+    void Start()
+    {
+        keyboard.ActiveMinSqrt = sensitivity.value * sensitivity.value;
+        keyboard.IgnoreMaxSqrt = (sensitivity.value + 0.05f) * (sensitivity.value + 0.05f);
+        keyboard.TriggerToInput = triggerToInput.isOn;
+    }
+}
---
+++ Assets/Keyboard/UI/SliderValueDisplay.cs
@@ -0,0 +1,14 @@
+﻿using System.Globalization;
+using UdonSharp;
+using UnityEngine.UI;
+
+public class SliderValueDisplay : UdonSharpBehaviour
+{
+    public Text text;
+    public Slider slider;
+
+    private void Update()
+    {
+        text.text = slider.value.ToString("F2", CultureInfo.InvariantCulture);
+    }
+}
