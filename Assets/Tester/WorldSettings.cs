8eb548ed610010be71d0b79e525dd83e32a6dff0
anatawa12 <anatawa12@icloud.com>
2022-07-12 14:53:08 +0900

    fix: set jump height

---
+++ Assets/Tester/WorldSettings.cs
@@ -0,0 +1,19 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class WorldSettings : UdonSharpBehaviour
+{
+    void Start()
+    {
+        if (Networking.LocalPlayer.IsValid())
+        {
+            Networking.LocalPlayer.SetJumpImpulse();
+            Networking.LocalPlayer.SetWalkSpeed();
+            Networking.LocalPlayer.SetRunSpeed();
+            Networking.LocalPlayer.SetStrafeSpeed();
+        }
+    }
+}
